2e66853c12429f51b8b7762b04b5768726c87f35
marcosrizzo8305-web <marcosrizzo8305@gmail.com>
2026-06-11 18:38:11 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function startCarrera() {
+  alert("Entrando en Modo Carrera...");
+}
+
+function startOnline() {
+  alert("Entrando en Modo Online...");
+}
+
+function openConfig() {
+  alert("Abriendo Configuración...");
+}
